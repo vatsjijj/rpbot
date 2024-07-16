252cf595145a771723d458ee487f6835f32f61c8
Azimuth <patricoconnor12@gmail.com>
2024-07-16 14:44:58 -0400

    Please

--- lib/character.dart
+++ lib/character.dart
@@ -5,4 +5,36 @@ class Character {
   List<String> debuffs = [], buffs = [];
 
   Character(this.name);
+
+  bool addBuff(String buff) {
+    if (!status.buffs.containsKey(buff)) {
+      return false;
+    }
+    buffs.add(buff);
+    return true;
+  }
+
+  bool addDebuff(String debuff) {
+    if (!status.debuffs.containsKey(debuff)) {
+      return false;
+    }
+    debuffs.add(debuff);
+    return true;
+  }
+
+  bool removeBuff(String buff) {
+    if (!buffs.contains(buff)) {
+      return false;
+    }
+    buffs.removeAt(buffs.indexOf(buff));
+    return true;
+  }
+
+  bool removeDebuff(String debuff) {
+    if (!debuffs.contains(debuff)) {
+      return false;
+    }
+    debuffs.removeAt(debuffs.indexOf(debuff));
+    return true;
+  }
 }
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+pkill watcher
 pkill rpbot
