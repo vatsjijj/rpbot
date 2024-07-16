e0407018431fc1811fb9a2f13c5a78e89068f370
Azimuth <patricoconnor12@gmail.com>
2024-07-16 12:22:52 -0400

    Simple watch scripts
    May be broken af :)

---
+++ scripts/watcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+count = 0
+
+until ./run.sh; do
+	((i++))
+	echo "Process died with exit code $?. Respawning." >&2
+	cd ..
+	mv log.txt ../logs/log$i.txt
+	cd scripts
+	sleep 1
+done
