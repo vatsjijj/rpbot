ae126c856f14c669052c767abfe972a0809f4a50
Azimuth <patricoconnor12@gmail.com>
2024-07-16 12:32:43 -0400

    Please

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -8,7 +8,7 @@ cd ..
 until (./rpbot > log.txt 2>&1); do
 	((i++))
 	echo "Process died with exit code $?. Respawning." >&2
-	mv log.txt ../logs/log$i.txt
+	mv log.txt "~/logs/log$i.txt"
 	cd scripts
 	sleep 1
 done
